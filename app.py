664f113545f8fcd9b0415c5153f8cfcc7713abcb
sreekanth-warrier <sreekanthwarrier.24@gmail.com>
2019-11-26 22:58:29 +0530

    flask codes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from db_config import DbConfig
-
+import time
 
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
@@ -14,17 +14,36 @@ db = SQLAlchemy(app)
 
 from db_model import *
 
+@app.route('/')
+def homePage():
+    return render_template('home.html')
+
 # Create a URL route in our application for "/"
 @app.route('/api/message', methods=['POST'])
-def newMessage():
-    #if request.method == 'POST':
-    message = request.form['text']
-    post = Post(message)
-    #print(Post)
+def addMsg():
+    text = request.form['text']
+    post = Post(text)
     db.session.add(post)
     db.session.commit()
     posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('home.html', posts=posts)
+    return render_template('output.html', posts=posts)
+
+@app.route('/api/message', methods=['GET'])
+def listMsg():
+    listall = Post.query.order_by(Post.text).all()
+    return render_template('output3.html', listall=listall)
+
+@app.route('/api/message/<msg_id>', methods=['GET'])
+def getMsgId(msg_id):
+    message = Post.query.get(msg_id).text
+    return render_template('output1.html', message=message)
+
+@app.route('/api/message/<msg_id>', methods=['DELETE'])
+def delMsg(msg_id):
+    delmessage = Post.query.filter_by(id=msg_id).first()
+    db.session.delete(delmessage)
+    db.session.commit()
+    return render_template('output2.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
--- db_model.py
+++ db_model.py
@@ -1,19 +1,16 @@
 import datetime
 from app import db
 
-
 class Post(db.Model):
 
     __tablename__ = 'posts'
-
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     text = db.Column(db.String, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False)
+    date_posted = db.Column(db.DateTime)
 
-    def __init__(self, text, id):
-        self.id = id
-        self.text = text
+    def __init__(self, text):
+	    self.text = text
         self.date_posted = datetime.datetime.now()
 
 
-#create table posts (id int, text varchar(255), date_posted varchar(255))
+
